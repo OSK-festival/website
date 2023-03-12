34ab4ce16051fc31f69b7965a925bae0d74fd250
yas-ako <105139975+yas-ako@users.noreply.github.com>
2023-03-12 18:18:47 +0900

    add: ESlint,Prettier

---
+++ eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+    // 'vue/setup-compiler-macros': true,
+  },
+  // parser: '@typescript-eslint/parser',
+  // parser: 'vue-eslint-parser',
+  extends: [
+    // -----------------------------------------------------------------
+    // 'plugin:@typescript-eslint/parser',
+    // 'plugin:vue/vue3-recommended',
+    // 'eslint:recommended',
+    // 'plugin:vue/vue3-essential',
+    // 'plugin:@typescript-eslint/recommended',
+    // '@vue/prettier',
+    // -----------------------------------------------------------------
+    '@nuxt/eslint-config',
+  ],
+  // parser: 'vue-eslint-parser',
+  // extends: [
+  //   'plugin:vue/base',
+  //   'eslint:recommended',
+  //   'plugin:vue/vue3-recommended',
+  //   'plugin:vue/essential',
+  //   'plugin:@typescript-eslint/recommended',
+  //   'plugin:@typescript-eslint/parser',
+  //   'plugin:prettier/recommended',
+  //   'eslint-config-prettier',
+  //   '@vue/prettier',
+  // ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  // plugins: ['vue', '@typescript-eslint'],
+  rules: {},
+}
