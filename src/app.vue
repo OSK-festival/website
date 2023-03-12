2a1e7f5f60414fb2b622f5dddf5fc2b9a992383d
yas-ako <105139975+yas-ako@users.noreply.github.com>
2023-03-12 12:57:19 +0900

    add: eslint, prettier

--- src/app.vue
+++ src/app.vue
@@ -1,14 +1,13 @@
 <template>
+  <div class="font-bold underline top-0 mx-2 my-4">Hello world!</div>
   <div>
-    <NuxtWelcome />
-    <h1
-      class="
-    text-3xl
-    font-bold
-    underline
-    "
-    >
-      Hello world!
-    </h1>
+    Counter:
+    {{ counter }}
+    <button @click="counter++">+</button>
+    <button @click="counter--">-</button>
   </div>
 </template>
+
+<script setup>
+const counter = useState('counter', () => Math.round(Math.random() * 1000))
+</script>
