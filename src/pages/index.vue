940a78e4e19d0f0a322b7c04e88eb3be42c04ba1
yas-ako <105139975+yas-ako@users.noreply.github.com>
2023-04-16 11:12:43 +0900

    update: 画像

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,83 +1,87 @@
 <template>
   <div>
-    <SectionAbout>
-      <template #heading>みだし</template>
-      <template #subHeading>みだしああ</template>
-      <template #description>みだしああ</template>
-    </SectionAbout>
-    <SectionAbout>
-      <template #heading>みだし</template>
-      <template #subHeading>みだしああ</template>
-    </SectionAbout>
-    <SectionAbout>
-      <template #heading>みだし</template>
-      <template #subHeading>みだしああ</template>
-    </SectionAbout>
-    <SectionAbout>
-      <template #heading>みだし</template>
-      <template #subHeading>みだしああ</template>
-    </SectionAbout>
-    <SectionAbout>
-      <template #heading>みだし</template>
-      <template #subHeading>
-        <SectionBorderBox>aa</SectionBorderBox>
-      </template>
-    </SectionAbout>
+    <TopImage />
+    <div>
+      <ArticleComponent />
+      <SectionAbout>
+        <template #heading>みだし</template>
+        <template #subHeading>みだしああ</template>
+        <template #description>みだしああ</template>
+      </SectionAbout>
+      <SectionAbout>
+        <template #heading>みだし</template>
+        <template #subHeading>みだしああ</template>
+      </SectionAbout>
+      <SectionAbout>
+        <template #heading>みだし</template>
+        <template #subHeading>みだしああ</template>
+      </SectionAbout>
+      <SectionAbout>
+        <template #heading>みだし</template>
+        <template #subHeading>みだしああ</template>
+      </SectionAbout>
+      <SectionAbout>
+        <template #heading>みだし</template>
+        <template #subHeading>
+          <SectionBorderBox>aa</SectionBorderBox>
+        </template>
+      </SectionAbout>
 
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
-    <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+      <div>aaaa</div>
+    </div>
   </div>
 </template>
