6cd7cac17c87bee1f53d5519b4b92d028d2e6327
yas-ako <105139975+yas-ako@users.noreply.github.com>
2023-04-16 11:13:17 +0900

    add: Article Component

---
+++ src/components/ArticleComponent.vue
@@ -0,0 +1,7 @@
+<template>
+  <article class="mx-auto w-3/5 border-2 border-solid border-black">
+    <div>ss</div>
+  </article>
+</template>
+
+<script setup></script>
---
+++ src/components/TopImage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="full -z-20 -mx-4 -mt-20"></div>
+</template>
+
+<script setup></script>
+
+<style>
+  .full {
+    position: relative;
+    width: 100%+1rem;
+    min-height: 100vh;
+    background: url('./images/school-1.jpg') center / cover;
+  }
+</style>
